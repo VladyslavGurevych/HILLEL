320114963c2fd3c168b501461a0c38fd4c8928d9
VladyslavGurevych <gurik1989@ukr.net>
2022-08-23 22:45:30 +0100

    Task 5. With out 'FOR' and 'IF'

--- HW_3/Task_5.py
+++ HW_3/Task_5.py
@@ -1,17 +1,7 @@
-# You have a group of people with non-unique names.
-# Generate a list of non-duplicate names (you cannot use set for this task.
-# If there are 2 johns in the list, you need to take only one of them.
-# "John Dow", "John Dow", "Marta Dow" => "John Dow", "Marta Dow ")
 
-list = ["Alex Bobin", "Vera Dorn", "Olga Lashukova", "Igor Petrov", "Vanya Rodin", "Ira Vlasova", "Igor Petrov"]
 
-non_duplicate = []
+# ___________________With out 'FOR' and 'IF'_______________
 
-for item in list:
-    if item in non_duplicate:
-        continue
-    else:
-        non_duplicate.append(item)
-
-print(non_duplicate)
-
+mylist = ["Alex Bobin", "Vera Dorn", "Olga Lashukova", "Igor Petrov", "Vanya Rodin", "Ira Vlasova", "Igor Petrov"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
