3de5ab294526c00a6e67815a4f987e639693e231
VladyslavGurevych <gurik1989@ukr.net>
2022-08-23 16:02:48 +0100

    Task 4. VIP box and common room

---
+++ HW_3/Task_4.py
@@ -0,0 +1,10 @@
+# You have a group of guests for the VIP box. The seats in the VIP box are all occupied by these guests.
+# There is a group of guests in the common room and there are still places in it. Display 2 groups of guests in code.
+
+vip_box_guests = ("Alex Bobin", "Vera Dorn", "Olga Lashukova", "Igor Petrov")
+common_room = ["Vanya Rodin", "Klara Bebach", "Ira Vlasova", "Igor Petrov", None, None]
+
+# None is free places for new guests))
+
+print(vip_box_guests)
+print(common_room)
